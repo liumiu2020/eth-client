12451b55b3574dd89f1f31e993f3603e054b876a
liumiu2020 <liumiu2020@gmail.com>
2025-11-22 12:05:18 +0800

    test

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 /*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2025-11-08 19:01:14
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-11-21 13:31:10
- * @FilePath: \go-client\main.go
+ * @Author: liumiu2020 liumiu2020@gmail.com
+ * @Date: 2025-11-21 21:34:25
+ * @LastEditors: liumiu2020 liumiu2020@gmail.com
+ * @LastEditTime: 2025-11-22 12:05:09
+ * @FilePath: \eth-client\main.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 package main
@@ -31,7 +31,7 @@ func main() {
 	if err != nil {
 		fmt.Println("env error" + err.Error())
 		return
-	} 
+	}
 	route.R = gin.Default()
 	route.RouteReg()
 	var port string = "8888"
